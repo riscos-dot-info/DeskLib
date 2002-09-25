6a3c3e17bf9fb5d834dbd1a7c06af7468b45a2be
ajps <>
2002-09-25 17:15:41 +0000

    File_Size now returns the size of image files as well as normal files.

--- !DLSources/Libraries/File/Size.c
+++ !DLSources/Libraries/File/Size.c
@@ -1,17 +1,17 @@
 /*
     ####             #    #     # #
-    #   #            #    #       #          The FreeWare C library for 
+    #   #            #    #       #          The FreeWare C library for
     #   #  ##   ###  #  # #     # ###             RISC OS machines
     #   # #  # #     # #  #     # #  #   ___________________________________
-    #   # ####  ###  ##   #     # #  #                                      
+    #   # ####  ###  ##   #     # #  #
     #   # #        # # #  #     # #  #    Please refer to the accompanying
     ####   ### ####  #  # ##### # ###    documentation for conditions of use
     ________________________________________________________________________
 
     File:    File.Size.c
     Author:  Copyright © 1993 Jason Williams, 1994 Tony Houghton
-    Version: 1.02 (24 Sep 1994)
-    Purpose: Reads the size of the given file
+    Version: 1.03 (25 Sep 2002)
+    Purpose: Reads the size of the given file.
 */
 
 #include "kernel.h"
@@ -21,9 +21,12 @@
 extern int File_Size(const char *filename)
 {
   _kernel_osfile_block osfile;
+  int object_type = _kernel_osfile(17, filename, &osfile);
 
-  if (_kernel_osfile(17, filename, &osfile) == 1)
+  /* Allow both image files and normal files to be "sized" */
+  if (object_type == 1 || object_type == 3)
     return (osfile.start);
+  /* Otherwise return the error code zero */
   else
     return (0);
 }
