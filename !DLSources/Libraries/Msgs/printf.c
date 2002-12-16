1b3f6eff621a2fcf7cf1e82d85b14afb33ab379c
takkaria <>
2002-12-16 18:36:20 +0000

    Made Msgs_printf() return a BOOL to indicate success (or failure).

--- !DLSources/Libraries/Msgs/printf.c
+++ !DLSources/Libraries/Msgs/printf.c
@@ -1,37 +1,36 @@
 /*
-    ####             #    #     # #
-    #   #            #    #       #          The FreeWare C library for 
-    #   #  ##   ###  #  # #     # ###             RISC OS machines
-    #   # #  # #     # #  #     # #  #   ___________________________________
-    #   # ####  ###  ##   #     # #  #                                      
-    #   # #        # # #  #     # #  #    Please refer to the accompanying
-    ####   ### ####  #  # ##### # ###    documentation for conditions of use
-    ________________________________________________________________________
-
-    File:    Msgs.printf.c
-    Author:  Copyright © 1992 Jason Williams
-    Version: 1.00 (08 Apr 1992)
-    Purpose: MessageTrans-like message handling functions.
-             (If you want MessageTrans, use the SWI interface, if you want
-             high-level message handling, use this code...)
-*/
+ * File: Msgs.printf.c
+ * Author: Jason Williams, 8th April 1992
+ *
+ * Abstract: MessageTrans-like message handling functions. (If you want
+ * MessageTrans, use the SWI interface, if you want high-level message
+ * handling, use this code...)
+ *
+ * This file is part of DeskLib, the general-purpose freeware C library for
+ * RISC OS.  Please refer to the accompanying documentation for conditions
+ * of use.
+ */
 
 #include <stdarg.h>
 #include <stdio.h>
 
 #include "DeskLib:Msgs.h"
 
-
-extern void Msgs_printf(char *result, char *formattag, ...)
+extern BOOL Msgs_printf(char *result, char *formattag, ...)
 {
   va_list ap;
   char temp[1024];
 
   result[0] = '\0';
+
   if (Msgs_Lookup(formattag, temp, 1020))
   {
     va_start(ap, formattag);
     vsprintf(result, temp, ap);
     va_end(ap);
+
+    return (TRUE);
   }
+
+  return (FALSE);
 }
