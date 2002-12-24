642ae28cb0d15742b247ea0878b2e4c0fab0881f
peter <peter>
2002-12-24 10:39:05 +0000

    Allow to compile, fix warnings and remove tabs (naughty takky)

--- !DLSources/Libraries/Url/Launch.c
+++ !DLSources/Libraries/Url/Launch.c
@@ -22,6 +22,13 @@
  */
 
 #include "DeskLib:Url.h"
+#include "DeskLib:Wimp.h"
+#include "DeskLib:WimpSWIs.h"
+#include "DeskLib:EventMsg.h"
+#include "DeskLib:SWI.h"
+
+#include <stdlib.h>
+#include <string.h>
 
 /* ANT URL broadcast wimp message number */  
 #define message_ANTOPENURL (message_action) 0x4AF80
@@ -36,9 +43,9 @@ static BOOL Url_ReturnMessage_h(event_pollblock *event, void *param);
 /* Internal data structure */
 typedef struct
 {
-	char *url;
-	urllaunch_handler *success;
-	urllaunch_handler *failure;
+  char *url;
+  urllaunch_handler *success;
+  urllaunch_handler *failure;
 } url_info_blk;
 
 /* ---------------------------------------------------------------------
@@ -46,23 +53,23 @@ typedef struct
  * --------------------------------------------------------------------- */
 static BOOL Url_Succeeded(event_pollblock *event, void *param)
 {
-	url_info_blk *block = (url_info_blk *) param;
-	UNUSED(event);
+  url_info_blk *block = (url_info_blk *) param;
+  UNUSED(event);
 
-	/* Release the various events we claimed to measuere success */
-	EventMsg_Release(message_URI_MRETURNRESULT, event_ANY, Url_ReturnMessage_h);
-	EventMsg_Release(message_ANTOPENURL, event_ANY, Url_ReturnMessage_h);
-	Event_Release(event_NULL, event_ANY, event_ANY, Url_Succeeded, param);
+  /* Release the various events we claimed to measuere success */
+  EventMsg_Release(message_URI_MRETURNRESULT, event_ANY, Url_ReturnMessage_h);
+  EventMsg_Release(message_ANTOPENURL, event_ANY, Url_ReturnMessage_h);
+  Event_Release(event_NULL, event_ANY, event_ANY, Url_Succeeded, param);
 
-	if (block->success != NULL)
-	{
-		block->success(block->url);
-	}
+  if (block->success != NULL)
+  {
+    block->success(block->url);
+  }
 
-	if (block->url != NULL) free(block->url);
-	free(block);
+  if (block->url != NULL) free(block->url);
+  free(block);
 
-	return (TRUE);
+  return (TRUE);
 }
 
 /* ---------------------------------------------------------------------
@@ -70,21 +77,21 @@ static BOOL Url_Succeeded(event_pollblock *event, void *param)
  * --------------------------------------------------------------------- */
 static void Url_Failed(url_info_blk *block)
 {
-	/* Release the various events we claimed to measuere success */
-	EventMsg_Release(message_URI_MRETURNRESULT, event_ANY, Url_ReturnMessage_h);
-	EventMsg_Release(message_ANTOPENURL, event_ANY, Url_ReturnMessage_h);
-	Event_Release(event_NULL, event_ANY, event_ANY, Url_Succeeded, block);
+  /* Release the various events we claimed to measuere success */
+  EventMsg_Release(message_URI_MRETURNRESULT, event_ANY, Url_ReturnMessage_h);
+  EventMsg_Release(message_ANTOPENURL, event_ANY, Url_ReturnMessage_h);
+  Event_Release(event_NULL, event_ANY, event_ANY, Url_Succeeded, block);
 
-	/* If there was a "failure" function given, call it now. */
-	if (block->failure != NULL) block->failure(block->url);
+  /* If there was a "failure" function given, call it now. */
+  if (block->failure != NULL) block->failure(block->url);
 
-	/* Free our copy of the URL we were calling */
-	if (block->url != NULL) free(block->url);
+  /* Free our copy of the URL we were calling */
+  if (block->url != NULL) free(block->url);
 
-	/* And the data block containing it and the callbacks */
-	free(block);
+  /* And the data block containing it and the callbacks */
+  free(block);
 
-	return;
+  return;
 }
 
 /* ---------------------------------------------------------------------
@@ -96,24 +103,24 @@ static void Url_Failed(url_info_blk *block)
  * --------------------------------------------------------------------- */
 static int Url_ANTBroadcast(const char *url)
 {
-	message_block msg;
+  message_block msg;
 
-	/* We don't bother trying for longer URLs */
-	if (strlen(url) > 235) return (-1);
+  /* We don't bother trying for longer URLs */
+  if (strlen(url) > 235) return (-1);
 
-	/* Prepare the wimp message header */
-	msg.header.size = ((sizeof(msg.header) + strlen(url) + 4) & ~3);
-	msg.header.yourref = 0;
-	msg.header.action = message_ANTOPENURL;
+  /* Prepare the wimp message header */
+  msg.header.size = ((sizeof(msg.header) + strlen(url) + 4) & ~3);
+  msg.header.yourref = 0;
+  msg.header.action = message_ANTOPENURL;
 
-	/* Write the URL string into the "data" part of the message */
-	msg.data.bytes[0] = 0;
-	strncat(msg.data.bytes, url, sizeof(msg.data.bytes) - 1);
+  /* Write the URL string into the "data" part of the message */
+  msg.data.bytes[0] = 0;
+  strncat(msg.data.bytes, url, sizeof(msg.data.bytes) - 1);
 
-	/* Attempt to broadcast the message, return an error if we fail */
-	if (Wimp_SendMessage(event_SENDWANTACK, &msg, 0 , 0)) return (-1);
+  /* Attempt to broadcast the message, return an error if we fail */
+  if (Wimp_SendMessage(event_SENDWANTACK, &msg, 0 , 0)) return (-1);
 
-	return (0);
+  return (0);
 }
 
 /* ---------------------------------------------------------------------
@@ -122,47 +129,47 @@ static int Url_ANTBroadcast(const char *url)
  * --------------------------------------------------------------------- */
 static BOOL Url_ANTLoad(const char *url)
 {
-	char *command_line = NULL;
-	char *colon;
+  char *command_line = NULL;
+  char *colon;
 
-	/* Find the protocol (method) delimiter */
-	colon = strchr(url, ':');
+  /* Find the protocol (method) delimiter */
+  colon = strchr(url, ':');
 
-	/* We've not found a ':', so we can't identify the protocol */
-	if (colon == NULL) return (FALSE);
+  /* We've not found a ':', so we can't identify the protocol */
+  if (colon == NULL) return (FALSE);
 
-	/* Work out how long the command line should be, allocate space */
-	command_line = malloc(14 + (colon - url) + 1 + strlen(url) + 1);
+  /* Work out how long the command line should be, allocate space */
+  command_line = malloc(14 + (colon - url) + 1 + strlen(url) + 1);
 
-	/* We couldn't allocate memory, so return failure */
-	if (command_line == NULL) return (FALSE);
+  /* We couldn't allocate memory, so return failure */
+  if (command_line == NULL) return (FALSE);
 
-	/* Build a system variable to read the contents of */
-	strcpy(command_line, "Alias$URLOpen_");
+  /* Build a system variable to read the contents of */
+  strcpy(command_line, "Alias$URLOpen_");
 
-	/* Append the "method" */
-	strncat(command_line, url, colon - url);
+  /* Append the "method" */
+  strncat(command_line, url, colon - url);
 
-	/* Terminate the string */
-	command_line[14 + (colon - url)] = 0;
+  /* Terminate the string */
+  command_line[14 + (colon - url)] = 0;
 
-	/* If we can't read the contents of the variable we can't launch any application */
-	if (getenv(command_line) == NULL) return (FALSE);
+  /* If we can't read the contents of the variable we can't launch any application */
+  if (getenv(command_line) == NULL) return (FALSE);
 
-	/* Add a space to our string */
-	strcat(command_line, " ");
+  /* Add a space to our string */
+  strcat(command_line, " ");
 
-	/* Write the rest of the URL into the buffer */
-	strcat(command_line, url);
+  /* Write the rest of the URL into the buffer */
+  strcat(command_line, url);
 
-	/* Try and Launch the task, passing the URL as parameter */
-	if (Wimp_StartTask(command_line + 6) != NULL)
-	{
-		/* Failure to start the task */
-		return (FALSE);
-	}
+  /* Try and Launch the task, passing the URL as parameter */
+  if (Wimp_StartTask(command_line + 6) != NULL)
+  {
+    /* Failure to start the task */
+    return (FALSE);
+  }
 
-	return (TRUE);
+  return (TRUE);
 }
 
 /* ---------------------------------------------------------------------
@@ -170,16 +177,16 @@ static BOOL Url_ANTLoad(const char *url)
  * --------------------------------------------------------------------- */
 static void Url_URIDispatch(const char *url)
 {
-	int flags;
+  int flags;
 
-	/* Dispatch the SWI, asking to be notified of the result with a message */ 
-	if (SWI(3, 1, SWI_URI_Dispatch, 1, url, event_taskhandle, &flags) != NULL || flags & 1)
-	{
-		/* If we have failed, move straight onto the "antload" technique */
-		Url_ANTLoad(url);
-	}
+  /* Dispatch the SWI, asking to be notified of the result with a message */ 
+  if (SWI(3, 1, SWI_URI_Dispatch, 1, url, event_taskhandle, &flags) != NULL || flags & 1)
+  {
+    /* If we have failed, move straight onto the "antload" technique */
+    Url_ANTLoad(url);
+  }
 
-	return;
+  return;
 }
 
 /* ---------------------------------------------------------------------
@@ -188,46 +195,46 @@ static void Url_URIDispatch(const char *url)
  * --------------------------------------------------------------------- */
 static BOOL Url_ReturnMessage_h(event_pollblock *event, void *param)
 {
-	url_info_blk *block = (url_info_blk *) param;
+  url_info_blk *block = (url_info_blk *) param;
 
-	/* This has come back from the Acorn system */
-	if (event->data.message.header.action == message_URI_MRETURNRESULT)
-	{
-		/* Check the flags to see if the URL was claimed */
-		if ((event->data.message.data.words[0] & 0x01) == 0)
-		{
-			/* The URL was claimed - we can stop now */
-			Url_Succeeded(NULL, param);
-			return (TRUE);
-		}
+  /* This has come back from the Acorn system */
+  if (event->data.message.header.action == message_URI_MRETURNRESULT)
+  {
+    /* Check the flags to see if the URL was claimed */
+    if ((event->data.message.data.words[0] & 0x01) == 0)
+    {
+      /* The URL was claimed - we can stop now */
+      Url_Succeeded(NULL, param);
+      return (TRUE);
+    }
 
-		/* failed AcornURI. Try ANT launch */
-		if (Url_ANTLoad(block->url) == FALSE)
-		{
-			Url_Failed(block);
-		}
-		else
-		{
-			Url_Succeeded(NULL, block);
-		}
+    /* failed AcornURI. Try ANT launch */
+    if (Url_ANTLoad(block->url) == FALSE)
+    {
+      Url_Failed(block);
+    }
+    else
+    {
+      Url_Succeeded(NULL, block);
+    }
 
-		/*
-		 * We haven't acknowledged the returnresult message, so the URI
-		 * handler task will automatically free the URI
-		 */
-	}
-	else if (event->data.message.header.action == message_ANTOPENURL)
-	{
-		/* If this was a response to the ant broadcast */
-		if (event->type == event_USERMESSAGEACK)
-		{
-			/* Try the acorn method instead */
-			Url_URIDispatch(block->url);
-			return (TRUE);
-		}
-	}
+    /*
+     * We haven't acknowledged the returnresult message, so the URI
+     * handler task will automatically free the URI
+     */
+  }
+  else if (event->data.message.header.action == message_ANTOPENURL)
+  {
+    /* If this was a response to the ant broadcast */
+    if (event->type == event_USERMESSAGEACK)
+    {
+      /* Try the acorn method instead */
+      Url_URIDispatch(block->url);
+      return (TRUE);
+    }
+  }
 
-	return (FALSE);
+  return (FALSE);
 }
 
 /* ---------------------------------------------------------------------
@@ -237,39 +244,39 @@ static BOOL Url_ReturnMessage_h(event_pollblock *event, void *param)
  * --------------------------------------------------------------------- */
 extern void Url_Launch(const char *url, urllaunch_handler *failure, urllaunch_handler *success)
 {
-	url_info_blk *block = NULL;
+  url_info_blk *block = NULL;
 
-	block = malloc(sizeof(url_info_blk));
-	if (block == NULL)
-	{
-		failure(url);
-		return;
-	}
+  block = malloc(sizeof(url_info_blk));
+  if (block == NULL)
+  {
+    failure(url);
+    return;
+  }
 
-	block->failure = failure;
-	block->success = success;
-	block->url = malloc(strlen(url) + 1);
+  block->failure = failure;
+  block->success = success;
+  block->url = malloc(strlen(url) + 1);
 
-	/* Take a copy of the URL */
-	if (block->url == NULL)
-	{
-		Url_Failed(block);
-		return;
-	}
-	else
-	{
-		strcpy(block->url, url);
-	}
+  /* Take a copy of the URL */
+  if (block->url == NULL)
+  {
+    Url_Failed(block);
+    return;
+  }
+  else
+  {
+    strcpy(block->url, url);
+  }
 
-	/* Claim these message before we've sent anything out */
-	EventMsg_Claim(message_URI_MRETURNRESULT, event_ANY, Url_ReturnMessage_h, block);
-	EventMsg_Claim(message_ANTOPENURL, event_ANY, Url_ReturnMessage_h, block);
+  /* Claim these message before we've sent anything out */
+  EventMsg_Claim(message_URI_MRETURNRESULT, event_ANY, Url_ReturnMessage_h, block);
+  EventMsg_Claim(message_ANTOPENURL, event_ANY, Url_ReturnMessage_h, block);
 
-	/* This is in case the original ANT broadcast succeeds! */
-	Event_Claim(event_NULL, event_ANY, event_ANY, Url_Succeeded, block);
+  /* This is in case the original ANT broadcast succeeds! */
+  Event_Claim(event_NULL, event_ANY, event_ANY, Url_Succeeded, block);
 
-	/* If this fails, we give an immediate report of problems */
-	if (Url_ANTBroadcast(url) == -1) Url_Failed(block);
+  /* If this fails, we give an immediate report of problems */
+  if (Url_ANTBroadcast(url) == -1) Url_Failed(block);
 
-	return;
+  return;
 }
