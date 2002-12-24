1b9d6e8cda096b9c1c8ced693ca60c1597afa2e1
Peter Naulls <>
2002-12-24 10:39:05 +0000

    Allow to compile, fix warnings and remove tabs (naughty takky)

--- !DLSources/Libraries/Url/Encode.c
+++ !DLSources/Libraries/Url/Encode.c
@@ -20,104 +20,104 @@
  * --------------------------------------------------------------------- */
 BOOL Url_Encode(const char *toencode, char *encoded_buf, int *bufsize)
 {
-	char *hex = "0123456789abcdef";
-	char *encoded = encoded_buf;
-	int okay = TRUE;
-	const char *encoding;
-	int counter;
-	int pass = 0;
+  const char *hex = "0123456789abcdef";
+  char *encoded = encoded_buf;
+  int okay = TRUE;
+  const char *encoding;
+  int counter;
+  int pass = 0;
 
-	/* Do two passes */
-	for (pass = 0; pass < 2; pass++)
-	{
-		/* Reset these */
-		counter = 0;
-		encoding = toencode;
+  /* Do two passes */
+  for (pass = 0; pass < 2; pass++)
+  {
+    /* Reset these */
+    counter = 0;
+    encoding = toencode;
 
-		/* Cycle through the string */
-		while (*encoding)
-		{
-			/* Reset */
-			okay = TRUE;
+    /* Cycle through the string */
+    while (*encoding)
+    {
+      /* Reset */
+      okay = TRUE;
 
-			/* Do ranges */
-			if (((*encoding >=   0) && (*encoding <=  31)) ||
-			    ((*encoding >= 128) && (*encoding <= 255)) ||
-			    ((*encoding >=  59) && (*encoding <=  64)) ||
-			    ((*encoding >=  34) && (*encoding <=  38)) ||
-			    ((*encoding >=  91) && (*encoding <=  96)) ||
-			    ((*encoding >= 123) && (*encoding <= 126)))
-			{
-				okay = FALSE;
-			}
-			else
-			{
-				/* Do individual values */
-				switch (*encoding)
-				{
-					case 32:
-					case 43:
-					case 44:
-					case 47:
-					{
-						okay = FALSE;
-					}
-				}
-			}
+      /* Do ranges */
+      if ((*encoding <=  31) ||
+         ((*encoding >= 128) && (*encoding <= 255)) ||
+         ((*encoding >=  59) && (*encoding <=  64)) ||
+         ((*encoding >=  34) && (*encoding <=  38)) ||
+         ((*encoding >=  91) && (*encoding <=  96)) ||
+         ((*encoding >= 123) && (*encoding <= 126)))
+      {
+        okay = FALSE;
+      }
+      else
+      {
+        /* Do individual values */
+        switch (*encoding)
+        {
+          case 32:
+          case 43:
+          case 44:
+          case 47:
+          {
+            okay = FALSE;
+          }
+        }
+      }
 
-			/* Check */
-			if (!okay)
-			{
-				/* If we are encoding to somewhere... */
-				if (pass == 1)
-				{
-					*(encoded++) = '%';
-	   				*(encoded++) = hex[(*encoding / 16) % 16];
-    				*(encoded++) = hex[*encoding % 16];
-				}
+      /* Check */
+      if (!okay)
+      {
+        /* If we are encoding to somewhere... */
+        if (pass == 1)
+        {
+          *(encoded++) = '%';
+             *(encoded++) = hex[(*encoding / 16) % 16];
+            *(encoded++) = hex[*encoding % 16];
+        }
 
-				/* Increase the needed size */
-				counter += 3;
-			}
-			/* Otherwise, just advance our pointer */
-			else
-			{
-				/* If we are encoding to somewhere, just set the next char */
-				if (pass == 1) *(encoded++) = *encoding;
+        /* Increase the needed size */
+        counter += 3;
+      }
+      /* Otherwise, just advance our pointer */
+      else
+      {
+        /* If we are encoding to somewhere, just set the next char */
+        if (pass == 1) *(encoded++) = *encoding;
 
-				/* Increase the needed size */
-				counter++;
-			}
+        /* Increase the needed size */
+        counter++;
+      }
 
-			/* Advance the read point */
-			encoding++;
-		}
+      /* Advance the read point */
+      encoding++;
+    }
 
-		/* We need one extra, for the terminator */
-		counter++;
+    /* We need one extra, for the terminator */
+    counter++;
 
-		/* Has the size been requested? */
+    /* Has the size been requested? */
         if (*bufsize == 0)
         {
-        	/* Set the size needed */
-        	*bufsize = counter;
+          /* Set the size needed */
+          *bufsize = counter;
 
-        	/* We've not filled in the URL (obviously) */
-        	return (FALSE);
+          /* We've not filled in the URL (obviously) */
+          return (FALSE);
         }
         else if (*bufsize < counter)
         {
-        	/* We've not got enough space */
-        	*bufsize = counter;
+          /* We've not got enough space */
+          *bufsize = counter;
 
-			/* Indicate this */
-        	return (FALSE);
-		}
-	}
+      /* Indicate this */
+          return (FALSE);
+    }
+  }
 
-	/* Terminate */
-	*encoded = '\0';
+  /* Terminate */
+  *encoded = '\0';
 
-	/* Done */
-	return (TRUE);
+  /* Done */
+  return (TRUE);
 }
