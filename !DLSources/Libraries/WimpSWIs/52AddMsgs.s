084a1cb8e1035f0f9955e27a8f2eb530369fd93c
ajps <ajps>
2002-12-28 17:59:07 +0000

    Added veneer for Wimp_AddMessages.

---
+++ !DLSources/Libraries/WimpSWIs/52AddMsgs.s
@@ -0,0 +1,46 @@
+;
+;       Title                  : Wimp Add Messages.
+;       System                 : Wimp Library
+;       Version                : 1.0
+;       Copyright              : (C) Antony Sidwell
+;       Date                   : 28th December, 2002
+;       Author                 : Antony Sidwell
+;
+;       Function               : Extends the set of user messages that the
+;                                Wimp sends to the task.
+;
+;       Modification history.
+;
+;       Version                : (Reflect in header IDENT)
+;       Date                   :
+;       Author                 :
+;       Changes                :
+;
+;
+;============================================================================
+;
+;  Include files.
+;
+;============================================================================
+;
+        GET     regdefs.h
+        GET     swinos.h
+        GET     macros.h
+;
+;============================================================================
+;
+;  Code.
+;
+;============================================================================
+;
+        PREAMBLE
+        STARTCODE Wimp_AddMessages
+;
+        MOV     ip, lr
+
+        SWI     SWI_Wimp_AddMessages + XOS_Bit
+        MOVVC   a1, #0
+
+        MOV     pc, ip
+;
+        END
