f0243a9aeea975d6f7a155ae49ef27799204f5c2
ajps <>
2003-01-15 18:48:12 +0000

    Added OS_Word code for Time_ReadClock

--- !DLSources/Libraries/Time/SwiNos.h
+++ !DLSources/Libraries/Time/SwiNos.h
@@ -1,6 +1,7 @@
 
 XOS_Bit                            EQU &020000
 
+SWI_OS_Word                        EQU &000007
 SWI_OS_ReadMonotonicTime           EQU &000042
 SWI_OS_ConvertStandardDateAndTime  EQU &0000C0
 SWI_OS_ConvertDateAndTime          EQU &0000C1
