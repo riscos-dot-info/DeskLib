6f4c2f5c48a8dd448c6d43a6653ef1421ed66415
ajps <ajps>
2003-01-15 17:25:16 +0000

    Added Wimp_RemoveMessages veneer.

---
+++ !DLSources/Libraries/WimpSWIs/53RmvMsgs.s
@@ -0,0 +1,46 @@
+;
+;       Title                  : Wimp Remove Messages.
+;       System                 : Wimp Library
+;       Version                : 1.0
+;       Copyright              : (C) Antony Sidwell
+;       Date                   : 15th January, 2002
+;       Author                 : Antony Sidwell
+;
+;       Function               : Removes messages from the set of user messages
+;                                that the Wimp sends to the task.
+;
+;       Modification history.
+;
+;       Version                : (Reflect in header IDENT)
+;       Date                   :
+;       Author                 :
+;       Changes                :
+;
+;
+;============================================================================
+;
+;  Include files.
+;
+;============================================================================
+;
+        GET     regdefs.h
+        GET     swinos.h
+        GET     macros.h
+;
+;============================================================================
+;
+;  Code.
+;
+;============================================================================
+;
+        PREAMBLE
+        STARTCODE Wimp_RemoveMessages
+;
+        MOV     ip, lr
+
+        SWI     SWI_Wimp_RemoveMessages + XOS_Bit
+        MOVVC   a1, #0
+
+        MOV     pc, ip
+;
+        END
