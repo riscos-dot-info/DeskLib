e3b02a0093659ece8d4b051d36094411180ddb05
ajps <ajps>
2003-01-15 07:15:01 +0000

    Added Time_ReadClock to read te 5-byte system clock

---
+++ !DLSources/Libraries/Time/ReadClock.s
@@ -0,0 +1,33 @@
+;   ####             #    #     # #
+;   #   #            #    #       #          The FreeWare C library for
+;   #   #  ##   ###  #  # #     # ###             RISC OS machines
+;   #   # #  # #     # #  #     # #  #   ___________________________________
+;   #   # ####  ###  ##   #     # #  #
+;   #   # #        # # #  #     # #  #    Please refer to the accompanying
+;   ####   ### ####  #  # ##### # ###    documentation for conditions of use
+;   ________________________________________________________________________
+;
+;   File:    KernelSWIs.s.ReadClock
+;   Author:  Copyright © 2003 Antony Sidwell
+;   Version: 1.00 (15 Jan 2003)
+;   Purpose: Reads the 5 byte time block.
+
+        GET     regdefs.h
+        GET     swinos.h
+        GET     macros.h
+
+; os_error *Time_ReadClock(char *fivebyteblock);
+
+        PREAMBLE
+        STARTCODE Time_ReadClock
+;
+        STMFD   sp!, {lr}
+        MOV     r1, r0
+        MOV     r0, #3
+        STRB    r0, [r1, #0]
+        MOV     r0, #14
+        SWI     SWI_OS_Word + XOS_Bit
+        MOVVC   r0, #0
+        LDMFD   sp!, {pc}
+;
+        END
