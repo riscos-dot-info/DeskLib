cebaea71912c2f0fc60a45616ba75551dceb40c1
ajps <ajps>
2003-01-15 20:10:46 +0000

    Added veneers for Territory_ConvertTimeToUTCOrdinals and Territory_ConvertTimeToOrdinals.

---
+++ !DLSources/Libraries/Time/ConvTTUTCO.s
@@ -0,0 +1,32 @@
+;   ####             #    #     # #
+;   #   #            #    #       #          The FreeWare C library for
+;   #   #  ##   ###  #  # #     # ###             RISC OS machines
+;   #   # #  # #     # #  #     # #  #   ___________________________________
+;   #   # ####  ###  ##   #     # #  #
+;   #   # #        # # #  #     # #  #    Please refer to the accompanying
+;   ####   ### ####  #  # ##### # ###    documentation for conditions of use
+;   ________________________________________________________________________
+;
+;   File:    KernelSWIs.s.ConvTTUTCO
+;   Author:  Copyright © 2003 Antony Sidwell
+;   Version: 1.00 (15 Jan 2003)
+;   Purpose: Converts five-byte time block to UTC  time ordinals
+
+        GET     regdefs.h
+        GET     swinos.h
+        GET     macros.h
+
+; os_error *Time_ConvertTimeToUTCOrdinals(char *fivebyteblock,
+;                                         time_ordinals *ord);
+
+        PREAMBLE
+        STARTCODE Time_ConvertTimeToUTCOrdinals
+;              
+        STMFD   sp!, {lr}
+        MOV     r2, r1
+        MOV     r1, r0
+        SWI     SWI_Territory_ConvertTimeToUTCOrdinals + XOS_Bit
+        MOVVC   a1, #0
+        LDMFD   sp!, {pc}
+;
+        END
