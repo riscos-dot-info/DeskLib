08a64684b57adbd20a8aa603d9e1d6b3a176a5fb
peter <peter>
2004-02-20 16:48:19 +0000

    Implement new header functionality

--- !DLSources/Libraries/Sprite/61RedirectM.s
+++ !DLSources/Libraries/Sprite/61RedirectM.s
@@ -1,12 +1,12 @@
 ;
-;       Title                  : Plot Scaled
+;       Title                  : Sprite 60N
 ;       System                 : Sprite Library
 ;       Version                : 1.0
-;       Copyright              : (C) Ainsley Pereira
-;       Date                   : Sun 27th February 94
-;       Author                 : Ainsley M. Pereira
+;       Copyright              : (C) John Winters
+;       Date                   : 26th January, 1992
+;       Author                 : John H. Winters
 ;
-;       Function               : Plots sprite scaled
+;       Function               : Issues an OS_SpriteOp 61 for a user sprite.
 ;
 ;
 ;       Modification history.
@@ -34,20 +34,15 @@
 ;============================================================================
 ;
         PREAMBLE
-        STARTCODE Sprite_PlotScaled
+        STARTCODE Sprite_RedirectMask
 ;
-        MOV       ip, sp
-        STMFD     sp!, {v1-v6, lr}
-        LDR       v4, [ip, #4]
-        MOV       v3, a4
-        MOV       v2, [ip, #0]
-        LDR       v1, [a3, #4]
-        LDR       a4, [a3, #0]
-        MOV       a3, a2
-        MOV       a2, a1
-        MOV       a1, #256 + 52
-        SWI       OS_SpriteOp + XOS_Bit
-        MOVVC     a1, #0
-        LDMFD     sp!, {v1-v6, pc}
+        STMFD   sp!, {a1, a2, a3, a4, lr}
+        LDMFD   sp!, {a2, a3, a4, ip}
+        MOV     a1, #256
+        ADD     a1, a1, #61
+        SWI     OS_SpriteOp + XOS_Bit
+        STMVCIA ip, {a1, a2, a3, a4}
+        MOVVC   a1, #0
+        LDMFD   sp!, {pc}
 ;
         END
