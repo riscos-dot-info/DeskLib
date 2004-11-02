3f24a2296e38e5755bc1986b0f013d548ae8a5cd
Peter Naulls <>
2004-11-02 14:30:50 +0000

    Window_GetInfo is bad news. Always use the GetInfo3 version

--- !DLSources/Libraries/Window/GetInfo.c
+++ !DLSources/Libraries/Window/GetInfo.c
@@ -9,38 +9,22 @@
     ________________________________________________________________________
 
     File:    Window.GetInfo.c
-    Author:  Copyright © 1992 Jason Williams
-    Version: 1.00 (19 Mar 1992)
+    Author:  Copyright © 1992 Jason Williams, Cy Booker
+    Version: 1.01 (26 Nov 1994)
     Purpose: High-level window management functions: Get Window info
+    Mods:    1.01 (26 Nov 1994) Jason Williams
+             1.02 (05 Mar 1995) Cy Booker
+                                now returns an (os_error *)
 */
 
-
-
-#include <string.h>
-
-#include "DeskLib:WimpSWIs.h"
 #include "DeskLib:Window.h"
+#include "DeskLib:WimpSWIs.h"
 
 
-extern void Window_GetInfo(window_handle window, window_info *result)
+extern os_error *Window_GetInfo(window_handle window, window_info *result)
 /* Returns just the window part of the window_info block (strips icon defn.s)
- * The real solution would be to find out how many icons were in the window
- * and then get a flex block big enough for the whole thing and then do it,
- * but you can't read the number of icons until you have read the window_info
- * so it's a bit difficult! As a result, I just use a 4kB block to hold
- * the window definition in.
- *
- *      NOTE THAT THIS WILL CRASH IF A WINDOW HAS TOO MANY ICONS!!!!!
- *      (anything more than about 120 icons will cause undefined results)
- *
- * SeeAlso: Window_GetInfo3()
  */
 {
-
-  int         block[1024];           /* 4096 bytes for winfo return block */
-  window_info *winfo = (window_info *) &block[0];
-
-  winfo->window = window;
-  Wimp_GetWindowInfo(winfo);
-  memcpy(result, winfo, sizeof(window_info));
+    result->window = window;
+    return Wimp_GetWindowInfo((window_info *)(void *)(((char *) result) + 1));
 }
