e721c767ba024f421ffadf15816959be0ca18bc9
Peter Naulls <>
2005-05-27 22:28:53 +0000

    Add Tinct

--- !DeskLib/include/SWI.h
+++ !DeskLib/include/SWI.h
@@ -383,6 +383,15 @@ os_error *SWI(int numregsin, int numregsout, int swicode, ... );
 #define SWI_PCI_RAMFree                    0x050391
 #define SWI_PCI_LogicalAddress             0x050392
 
+/* Tinct */
+#define SWI_Tinct_PlotAlpha                0x057240
+#define SWI_Tinct_PlotScaledAlpha          0x057241
+#define SWI_Tinct_Plot                     0x057242
+#define SWI_Tinct_PlotScaled               0x057243
+#define SWI_Tinct_ConvertSprite            0x057244
+#define SWI_Tinct_AvailableFeatures        0x057245
+#define SWI_Tinct_Compress                 0x057246
+#define SWI_Tinct_Decompress               0x057247
 
 #ifdef __cplusplus
 }
