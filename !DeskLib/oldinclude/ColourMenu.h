7c91e53b02041830c6ba2f6fac051ba6d4721fed
adam <adam>
2007-10-09 21:17:10 +0000

    Reorganised DeskLib$Path to move compatibility headers into seperate directory to exclude them from documentation

---
+++ !DeskLib/oldinclude/ColourMenu.h
@@ -0,0 +1,39 @@
+/*
+    ####             #    #     # #
+    #   #            #    #       #          The FreeWare C library for
+    #   #  ##   ###  #  # #     # ###             RISC OS machines
+    #   # #  # #     # #  #     # #  #   ___________________________________
+    #   # ####  ###  ##   #     # #  #
+    #   # #        # # #  #     # #  #    Please refer to the accompanying
+    ####   ### ####  #  # ##### # ###    documentation for conditions of use
+    ________________________________________________________________________
+
+    File:    ColourMenu.ColourMenu.c
+    Author:  Copyright © 1994 Craig Beech
+    Version: 1.01 (17 Apr 2005)
+    Purpose: Equivalent of the RISC_OSLib colourmenu function.
+*/
+
+#ifndef __dl_colourmenu_h
+#define __dl_colourmenu_h
+
+#ifndef __dl_menu_h
+#include "DeskLib:Menu.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Abstract */
+/*
+  Compatibility header. ColourMenu_Create has been renamed
+  dl_Menu_NewColourMenu and moved to the menu module
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
