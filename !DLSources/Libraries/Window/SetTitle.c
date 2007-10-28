83cc4dad4e7e451e1a3a14120d1cd0b06881e777
Adam Richardson <riscos@snowstone.org.uk>
2007-10-28 12:52:17 +0000

    Minor tweak to improve window title update on nested wimp & higher

--- !DLSources/Libraries/Window/SetTitle.c
+++ !DLSources/Libraries/Window/SetTitle.c
@@ -11,6 +11,7 @@
     File:    Window.c.SetTitle
     Author:  Copyright © 1993 Jason Williams
     Version: 1.00 (28 Jun 1993)
+             1.01 (28 Oct 2007) Uses new nested-wimp ForceRedraw to update title, if available.
     Purpose: Set the text in a window's titlebar, and force a redraw of
              the *correct* region of the screen to update it.
              (i.e. it even works with Edouard Poor's MegaWindow toolsprites!)
@@ -21,7 +22,8 @@
 #include "DeskLib:Wimp.h"
 #include "DeskLib:WimpSWIs.h"
 #include "DeskLib:Window.h"
-
+#include "DeskLib:SWI.h"
+#include "DeskLib:Event.h"
 
 
 extern void Window_SetTitle(window_handle window, const char *title)
@@ -47,17 +49,25 @@ extern void Window_SetTitle(window_handle window, const char *title)
 
   if (info.block.flags.data.open)   /* *IF* window open, redraw the titlebar */
   {
-    /* Get the position of the window on screen */
-    outline.window = window;
-    Wimp_GetWindowOutline(&outline);
+    if (event_wimpversion < 380)
+    {
+      /* Get the position of the window on screen */
+      outline.window = window;
+      Wimp_GetWindowOutline(&outline);
 
-    /*  Force a redraw of the title bar - it is the rectangle that lies
-     *  between the window outline as returned by Wimp_GetWindowOutline and
-     *  the window screenrect as returned by Wimp_GetWindowInfo
-     */
-    redraw.window     = -1;                        /* invalidate screen area */
-    redraw.rect       = outline.screenrect;
-    redraw.rect.min.y = info.block.screenrect.max.y;
-    Wimp_ForceRedraw(&redraw);
+      /*  Force a redraw of the title bar - it is the rectangle that lies
+       *  between the window outline as returned by Wimp_GetWindowOutline and
+       *  the window screenrect as returned by Wimp_GetWindowInfo
+       */
+      redraw.window     = -1;                        /* invalidate screen area */
+      redraw.rect       = outline.screenrect;
+      redraw.rect.min.y = info.block.screenrect.max.y;
+      Wimp_ForceRedraw(&redraw);
+    }
+    else
+    {
+      /* Nested wimp provides direct means of updating title bar */
+      SWI(3, 0, 0x400D1/*Wimp_ForceRedraw*/, window, 0x4B534154/*"TASK"*/, 3);
+    }
   }
 }
