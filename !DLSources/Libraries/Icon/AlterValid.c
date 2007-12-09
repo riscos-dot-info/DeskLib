1a6d805b664f063c459bd8721410d91723df760d
Adam Richardson <riscos@snowstone.org.uk>
2007-12-09 11:11:44 +0000

    Minor tweaks to Icon_AlterValidation to help programmer avoid buffer overflow

--- !DLSources/Libraries/Icon/AlterValid.c
+++ !DLSources/Libraries/Icon/AlterValid.c
@@ -12,6 +12,7 @@
     Author:  Copyright 1994 Tony Howat
     Version: 1.00 (2 Aug 1994)
              1.01 (5 Sep 2007) Changed strcpy to strncpy
+             1.02 (9 Dec 2007) Updated to return int with useful info
     Purpose: Changes an icon's validation string - makes pressy slabbed
              borders easy.
 */
@@ -20,21 +21,42 @@
 
 #include "DeskLib:WimpSWIs.h"
 #include "DeskLib:Icon.h"
+#include "DeskLib:Str.h"
 
 
-extern void Icon_AlterValidation(window_handle window, icon_handle icon,
+int Icon_AlterValidation(window_handle window, icon_handle icon,
                                  const char *newvalidation)
 {
   icon_block wimpicon;
+  int diff, len_existing;
 
   Wimp_GetIconState(window, icon, &wimpicon);
 
-  if (!strcmp(wimpicon.data.indirecttext.validstring, newvalidation))
-     return;
+  /* Only proceed if icon could possibly have a validation string */
+  if (!wimpicon.flags.data.indirected)
+    return 0;
+  else
+  {
+    /* Get length of existing validation string, for info */
+    len_existing = strlencr(wimpicon.data.indirecttext.validstring);
 
-  strncpy(wimpicon.data.indirecttext.validstring, newvalidation, sizeof(wimpicon.data.indirecttext.validstring)-1);
-  wimpicon.data.indirecttext.validstring[sizeof(wimpicon.data.indirecttext.validstring)-1] = '\0';
+    /* If user called us only to find length of existing string, return here */
+    if (newvalidation == NULL)
+      return len_existing;
 
-  Wimp_SetIconState(window, icon, 0, 0);
+    /* Check old vs. new strings are different - if not, do nothing */
+    if (!strcmpcr(wimpicon.data.indirecttext.validstring, newvalidation))
+       return 0;
+
+    /* Return difference between strings, for info */
+    diff = len_existing - strlencr(newvalidation);
+
+    /* Note: nothing to prevent buffer overrun */
+    strcpy(wimpicon.data.indirecttext.validstring, newvalidation);
+
+    Wimp_SetIconState(window, icon, 0, 0);
+  }
+
+  return diff;
 }
 
